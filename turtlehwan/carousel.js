9af646cb1b7f21dc51eac5495bc449607b5b9283
kim jihwan <kjhwan0802@naver.com>
2023-11-06 19:38:44 +0900

    carousel

---
+++ turtlehwan/carousel.js
@@ -0,0 +1,26 @@
+//참고 : https://codepen.io/letsBeCodeGenius/pen/PobQRXy
+// https://velog.io/@grinding_hannah/JavaScript-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EB%A1%9C-%EC%BA%90%EB%9F%AC%EC%85%80Carousel-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+
+const container = document.querySelector(".carousel-container");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+(function addEvent() {
+  prevBtn.addEventListener('click', translateContainer.bind(this, 1));
+  nextBtn.addEventListener('click', translateContainer.bind(this, -1));
+})();
+
+function translateContainer(direction) {
+  const selectedBtn = (direction === 1) ? 'prev' : 'next';
+  container.ontransitionend = () => reorganizeEl(selectedBtn);
+  container.style.transitionDuration = '500ms';
+  container.style.transform = `translateX(${direction * (100 / 5)}%)`;
+}
+
+function reorganizeEl(selectedBtn) {
+  container.removeAttribute('style');
+  if (selectedBtn === 'prev')
+    container.insertBefore(container.lastElementChild, container.firstElementChild);
+  else
+    container.appendChild(container.firstElementChild);
+}
